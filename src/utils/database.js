37907e18dd0ad332e95efb645e19c6f207fc5117
Kilian <gitlab.icg@gmail.com>
2024-03-14 22:07:36 +0100

    feat (bdd) : Création de la base de donnée par défaut

---
+++ src/utils/database.js
@@ -0,0 +1,21 @@
+const sqlite3 = require('sqlite3').verbose();
+let sql;
+
+// Connection à la base de données
+const db = new sqlite3.Database('db.sqlite', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
+
+sql = `
+    
+`;
+
+db.run(sql, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Tables created');
+});
