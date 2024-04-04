da557bd672fd44269fee91a537ff0c229128b5e5
guenk <gueguenk@gmail.com>
2024-04-04 18:50:31 +0200

    adding authentification to GamerRoutes and routes to Login/Register

--- backend/src/routes/GamerRoutes.js
+++ backend/src/routes/GamerRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const GamerController = require("../controllers/GamerController.js");
 const router = express.Router();
+const { authenticateToken } = require('./middlewares/Auth');
 
-router.get("/gamer/:id", GamerController.getById);
-router.put("/gamer/:id", GamerController.updateGamer);
-router.delete("/gamer/:id", GamerController.deleteGamer);
+router.get("/gamer/:id", authenticateToken, GamerController.getById);
+router.put("/gamer/:id", authenticateToken, GamerController.updateGamer);
+router.delete("/gamer/:id", authenticateToken, GamerController.deleteGamer);
 
 module.exports = router;
