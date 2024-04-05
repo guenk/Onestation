7264c03211ae5da9ad6fde292d2276bc4d02a653
Marion-Chedotal <marion.chedotal@gmail.com>
2024-04-05 11:32:41 +0200

    feat(game): add model and controller

---
+++ backend/src/models/GameModel.js
@@ -0,0 +1,23 @@
+const db = require("../utils/database.js");
+
+class GamerModel {
+  /**
+   * Afficher les infos du jeu
+   * @param {number} idGame - L'identifiant du jeu.
+   * @returns {Promise<Object>} Une promesse contenant les informations du jeu .
+   */
+  static findById(idGame) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM Game WHERE idGame = ?";
+      db.get(query, [idGame], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+}
+
+module.exports = GamerModel;
--- backend/src/routes/GamerRoutes.js
+++ backend/src/routes/GamerRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const GamerController = require("../controllers/GamerController.js");
 const router = express.Router();
-const { authenticateToken } = require('./middlewares/Auth');
+const { authenticateToken } = require('../middlewares/Auth');
 
 router.get("/gamer/:id", authenticateToken, GamerController.getById);
 router.put("/gamer/:id", authenticateToken, GamerController.updateGamer);
