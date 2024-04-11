e705ab27fc9e74e74405c013b51b37f35300df7e
Kilian <gitlab.icg@gmail.com>
2024-04-11 21:53:49 +0200

    feat (socket) : Ajout du chat général

--- backend/index.js
+++ backend/index.js
@@ -28,47 +28,61 @@ app.listen(port, () => {
 
 
 const gameRooms = new Map([
-    [1, {
-        users: [ 'P9WFZD1nL7ZMkclAAAB' ],
-        privateOrNot: false,
-        maxUsers: 12,
-        creator: [ 'P9WFZD1nL7ZMkclAAAB' ]
+    [0, {
+        users: [ 'Kilian' ], // Liste d'utilisateurs de la salle de jeux
+        privateOrNot: false, // Salle de jeux publique
+        maxUsers: 12, // Nombre d'utilisateurs autorisés
+        creator: [ 'Kilian' ] // ID de l'utilisateur/la socket ayant créé la salle de jeux
     }],
+    [1, {
+        users : [ 'Jean Patrick' ],
+        privateOrNot: false,
+        maxUsers: 10,
+        creator: [ 'Jean Patrick' ]
+    }]
 ]);
 
 // Connexion du joueur à l'app
 io.on("connection", (socket) => {
 
+    // Joindre le chat publique à la connexion
+    socket.on('home_room', () => {
+        socket.join(0);
+        gameRooms.get(0).users.push(socket.id);
+        socket.emit("home_room_joined", { roomID: 0, roomJoined: gameRooms.get(0) });
+    })
+
     // Création d'une partie
-    socket.on("create_game_room", ({privateOrNot}) => {
+    socket.on("create_game_room", ({profil, privateOrNot}) => {
         let roomID;
 
         do {
             roomID = Math.round(Math.random() * 100);
         } while (gameRooms.has(roomID));
 
-        gameRooms.set(roomID, { users: [socket.id], privateOrNot: privateOrNot, maxUsers: 10, creator: socket.id });
+        gameRooms.set(roomID, { users: [profil.username], privateOrNot: privateOrNot, maxUsers: 10, creator: socket.id });
 
         socket.join(roomID);
         socket.emit("game_room_created", {roomID, roomCreated: gameRooms.get(roomID)});
     })
 
     // Joindre une game publique aléatoire
-    socket.on("join_random_room", () => {
-        for (const index of gameRooms.keys()) {
-            const room = gameRooms.get(index);
+    socket.on("join_random_room", (profil) => {
+        for (const roomID of gameRooms.keys()) {
+            const room = gameRooms.get(roomID);
 
             if (! room.privateOrNot && (room.users.length + 1 <= room.maxUsers)) {
-                socket.join(index);
-                socket.emit("random_room_joined", { roomID: index, roomJoined: room });
+                socket.join(roomID);
+                room.users.push(profil.username);
+                socket.emit("random_room_joined", { roomID, roomJoined: room });
                 break;
             }
         }
     })
 
     // Envoi du message au reste des personnes dans la partie
-    socket.on("message", ({ message, roomID }) => {
-        socket.to(roomID).emit("receiveMessage", { receivedMessage: message });
+    socket.on("message", ({ message, roomID, profil }) => {
+        socket.to(roomID).emit("receiveMessage", { receivedMessage: message, sender: profil });
     });
 })
 
