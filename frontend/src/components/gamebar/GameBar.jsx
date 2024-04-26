399a3cb2b23dc74255860e6468cfa46fcd3abcc9
Kilian <kilianmalherbe.pro@gmail.com>
2024-04-26 09:09:33 +0200

    feat (etape): L'utilisateur choisit de façon random pour maintenant choisir dans une liste de 3 mot généré de façon random, le résultat est passé sur toutes les sockets

--- frontend/src/components/gamebar/GameBar.jsx
+++ frontend/src/components/gamebar/GameBar.jsx
@@ -1,10 +1,35 @@
-const GameBar = ({round, label}) => {
-    return (
+const GameBar = ({ round, label, etape, words, socket, player }) => {
+  if (etape === 2) {
+    if (socket.id === player.id) {
+      return (
         <div id="game-bar" className="flex justify-center gap-10">
-            <p>Round {round} sur 3</p>
-            <p>{label}</p>
+          <p>Round {round} sur 3</p>
+          <div>
+            <p>Dessinez ce mot</p>
+            <h5 className="text-center font-bold">{words}</h5>
+          </div>
         </div>
-    );
+      );
+    } else {
+      return (
+        <div id="game-bar" className="flex justify-center gap-10">
+          <p>Round {round} sur 3</p>
+          <div>
+            <p>Devinez ce mot</p>
+            <h5 className="font-bold text-center">
+              {[...words].map((letter, index) => {
+                if (letter === " ") {
+                  return <span key={index}>&nbsp;&nbsp;</span>;
+                } else {
+                  return " _ ";
+                }
+              })}
+            </h5>
+          </div>
+        </div>
+      );
+    }
+  }
 };
 
 export default GameBar;
--- frontend/src/components/gametoolbar/GameToolbar.jsx
+++ frontend/src/components/gametoolbar/GameToolbar.jsx
@@ -1,19 +1,29 @@
-const GameToolbar = ({roomID}) => {
-    async function copyToClipboard ()  {
-        try {
-            await navigator.clipboard.writeText("localhost:5173/?" + roomID);
-            setChatMessageAuto("Lien d'invitation copié");
-        } catch (error) {
-            console.log(error)
-        }
+const GameToolbar = ({ roomID, setChatMessageAuto }) => {
+  async function copyToClipboard() {
+    try {
+      await navigator.clipboard.writeText("localhost:5173/?" + roomID);
+      setChatMessageAuto("Lien d'invitation copié");
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        <div id="game-toolbar" className="rounded-xl overflow-hidden flex">
-            <input className="bg-slate-100 p-4 grow" type="text" value={"localhost:5173/?" + roomID} readOnly/>
-            <button className="bg-blue-300 p-4 font-bold text-white" onClick={copyToClipboard}>Copier</button>
-        </div>
-    );
+  return (
+    <div id="game-toolbar" className="rounded-xl overflow-hidden flex">
+      <input
+        className="bg-slate-100 p-4 grow"
+        type="text"
+        value={"localhost:5173/?" + roomID}
+        readOnly
+      />
+      <button
+        className="bg-blue-300 p-4 font-bold text-white hover:bg-blue-400 active:bg-blue-500"
+        onClick={copyToClipboard}
+      >
+        Copier
+      </button>
+    </div>
+  );
 };
 
 export default GameToolbar;
