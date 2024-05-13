8334b7643eaed98dcb993adaded26bc755858706
guenk <gueguenk@gmail.com>
2024-05-13 12:22:20 +0200

    adding avatar in front

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -9,126 +9,134 @@ import { Tooltip } from "react-tooltip";
 import "./style.scss";
 
 const navigation = [
-  { name: "Rejoindre une partie", href: "#", current: false },
-  { name: "Créer une partie", href: "#", current: false },
+	{ name: "Rejoindre une partie", href: "#", current: false },
+	{ name: "Créer une partie", href: "#", current: false },
 ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+	return classes.filter(Boolean).join(" ");
 }
 
 export default function Header() {
-  const { isAuthenticated, user } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+	const { isAuthenticated, user } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
 
-  const handleLogout = async () => {
-    try {
-      dispatch(logout());
-    } catch (error) {
-      console.error("Error during logout:", error);
-    }
-  };
+	const handleLogout = async () => {
+		try {
+			dispatch(logout());
+		} catch (error) {
+			console.error("Error during logout:", error);
+		}
+	};
+	const avatarFileName = user.avatar.split("/").pop();
+	return (
+		<Disclosure as="nav">
+			{({ open }) => (
+				<>
+					<div className="mx-auto max-w-7xl py-5 sm:px-6 lg:px-8 NavBar">
+						<div className="relative flex items-center">
+							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden pl-5">
+								{/* Mobile menu button*/}
+								<Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-black-400 pl-5">
+									<span className="absolute -inset-0.5" />
+									{open ? (
+										<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+									) : (
+										<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+									)}
+								</Disclosure.Button>
+							</div>
+							<div className="flex flex-1 items-center justify-center sm:justify-evenly">
+								<div className="flex flex-shrink-0 items-center">
+									<img
+										className="h-24 w-auto"
+										src={logo}
+										alt="guess my draw logo"
+									/>
+								</div>
+								<div className="hidden sm:ml-6 sm:block flex flex-col">
+									<div className="flex space-x-4">
+										{navigation.map((item) => (
+											<a
+												key={item.name}
+												href={item.href}
+												className="hover:underline hover:text-[#0B8DFD] hover:font-bold"
+											>
+												{item.name}
+											</a>
+										))}
+									</div>
+								</div>
 
-  return (
-    <Disclosure as="nav">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl py-5 sm:px-6 lg:px-8 NavBar">
-            <div className="relative flex items-center">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden pl-5">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-black-400 pl-5">
-                  <span className="absolute -inset-0.5" />
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:justify-evenly">
-                <div className="flex flex-shrink-0 items-center">
-                  <img className="h-24 w-auto" src={logo} alt="guess my draw logo" />
-                </div>
-                <div className="hidden sm:ml-6 sm:block flex flex-col">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="hover:underline hover:text-[#0B8DFD] hover:font-bold"
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
+								{isAuthenticated ? (
+									<div className="absolute inset-y-0 right-0 flex flex-col md:flex-row items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-3">
+										{user && (
+											<Link to={`/profil/${user?.id_gamer}`}>
+												<div
+													className="flex items-center gap-2"
+													data-tip
+													data-tooltip-id="tooltip-profil"
+													data-tooltip-content="Voir mon profil"
+												>
+													<img
+														className="h-8 w-8 rounded-full"
+														src={`${
+															import.meta.env.VITE_BACKEND_URL
+														}/static/${avatarFileName}`}
+														alt="Description de l'image"
+													/>
+													<p>
+														<strong>{user.pseudo}</strong>
+													</p>
+													<Tooltip id="tooltip-profil" effect="solid"></Tooltip>
+												</div>
+											</Link>
+										)}
+										<button
+											onClick={handleLogout}
+											className="font-bold authButton"
+										>
+											Déconnexion
+										</button>
+									</div>
+								) : (
+									<div className="absolute inset-y-0 right-0 flex flex-col md:flex-row items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-2">
+										<Link to="/login">
+											<button className="font-bold authButton">
+												Se connecter
+											</button>
+										</Link>
+										<Link to="/register">
+											<button className="font-bold authButton">
+												S'inscrire
+											</button>
+										</Link>
+									</div>
+								)}
+							</div>
+						</div>
+					</div>
 
-                {isAuthenticated ? (
-                  <div className="absolute inset-y-0 right-0 flex flex-col md:flex-row items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-3">
-                    {user && (
-                      <Link to={`/profil/${user?.id_gamer}`}>
-                        <div
-                          className="flex items-center gap-2"
-                          data-tip
-                          data-tooltip-id="tooltip-profil"
-                          data-tooltip-content="Voir mon profil"
-                        >
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src={user.avatar}
-                            alt="avatar"
-                          />
-                          <p>{user.pseudo}</p>
-                          <Tooltip id="tooltip-profil" effect="solid"></Tooltip>
-                        </div>
-                      </Link>
-                    )}
-                    <button
-                      onClick={handleLogout}
-                      className="font-bold authButton"
-                    >
-                      Déconnexion
-                    </button>
-                  </div>
-                ) : (
-                  <div className="absolute inset-y-0 right-0 flex flex-col md:flex-row items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-2">
-                    <Link to="/login">
-                      <button className="font-bold authButton">
-                        Se connecter
-                      </button>
-                    </Link>
-                    <Link to="/register">
-                      <button className="font-bold authButton">
-                        S'inscrire
-                      </button>
-                    </Link>
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? "bg-black-900 text-white" : "text-black-300",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  );
+					<Disclosure.Panel className="sm:hidden">
+						<div className="space-y-1 px-2 pb-3 pt-2">
+							{navigation.map((item) => (
+								<Disclosure.Button
+									key={item.name}
+									as="a"
+									href={item.href}
+									className={classNames(
+										item.current ? "bg-black-900 text-white" : "text-black-300",
+										"block rounded-md px-3 py-2 text-base font-medium"
+									)}
+									aria-current={item.current ? "page" : undefined}
+								>
+									{item.name}
+								</Disclosure.Button>
+							))}
+						</div>
+					</Disclosure.Panel>
+				</>
+			)}
+		</Disclosure>
+	);
 }
--- frontend/src/pages/profil/Profil.jsx
+++ frontend/src/pages/profil/Profil.jsx
@@ -9,105 +9,108 @@ import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { logout } from "../../redux/authActions.js";
 
 const Profil = () => {
-  const { user, token } = useSelector((state) => state.auth);
-  const [showModal, setShowModal] = useState(false);
+	const { user, token } = useSelector((state) => state.auth);
+	const [showModal, setShowModal] = useState(false);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const avatarFileName = user.avatar.split("/").pop();
 
-  //modal management
-  const handleShow = () => setShowModal(true);
+	//modal management
+	const handleShow = () => setShowModal(true);
 
-  const handleClose = () => {
-    setShowModal(false);
-  };
-  // delete management
-  const handleDelete = async (e) => {
-    e.preventDefault();
-    try {
-      await fetch(`http://localhost:3001/api/gamer/${user.id_gamer}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      dispatch(logout());
-      navigate("/");
-    } catch (err) {
-      console.error("Error deleting user:", err);
-    }
-  };
+	const handleClose = () => {
+		setShowModal(false);
+	};
+	// delete management
+	const handleDelete = async (e) => {
+		e.preventDefault();
+		try {
+			await fetch(`http://localhost:3001/api/gamer/${user.id_gamer}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			dispatch(logout());
+			navigate("/");
+		} catch (err) {
+			console.error("Error deleting user:", err);
+		}
+	};
 
-  return (
-    <div>
-      <Header />
-      <div className="bg-profil">
-        <div className="font-bold text-center text-2xl mt-20 ">
-          <h2>
-            <span className="text-[#FE2C65]">Bienvenue </span>
-            <span className="text-[#FFB401]">sur Guess </span>
-            <span className="text-[#0B8DFD]">My Draw !</span>
-          </h2>
-          <h2 className="text-[#FE2C65] my-16">Mon compte</h2>
-        </div>
-        <div className="mt-6 mx-auto max-w-xl bg-[#F7F6F6] rounded-3xl p-5 flex justify-between">
-          <div className="flex items-center">
-            <div className="w-40 h-40 overflow-hidden rounded-full">
-              <img
-                src={user?.avatar}
-                alt="avatar"
-                className="h-full w-full object-cover object-center"
-              />
-            </div>
-            <div className="text-lg ml-6">
-              <p className="pb-2">
-                Pseudo: <span className="text-gray-500">{user?.pseudo}</span>{" "}
-              </p>
-              <p className="pb-2">
-                Email: <span className="text-gray-500">{user?.email}</span>
-              </p>
-              <p>
-                Mot de passe:{" "}
-                <span className="text-gray-500">************</span>{" "}
-              </p>
-            </div>
-          </div>
-          <div className="self-top text-2xl">
-            <Link to={`/profil/modification/${user?.id_gamer}`}>
-              <button>
-                <FontAwesomeIcon icon={faPen} className="text-[#0B8DFD] me-5" />
-              </button>
-              <button onClick={handleShow}>
-                <FontAwesomeIcon icon={faTrash} className="text-[#FE2C65]" />
-              </button>
-            </Link>
-            {showModal && (
-              <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
-                <div className="bg-white p-8 rounded-lg text-sm">
-                  <p>Êtes-vous sûr de vouloir supprimer votre compte ?</p>
-                  <div className="mt-10 flex justify-end">
-                    <button
-                      className="bg-[#FE2C65] text-white px-4 py-2 rounded mr-4 hover:font-bold"
-                      onClick={handleDelete}
-                    >
-                      Supprimer
-                    </button>
-                    <button
-                      className="bg-[#FFB401] text-white px-4 py-2 rounded hover:font-bold"
-                      onClick={handleClose}
-                    >
-                      Annuler
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<div className="bg-profil">
+				<div className="font-bold text-center text-2xl mt-20 ">
+					<h2>
+						<span className="text-[#FE2C65]">Bienvenue </span>
+						<span className="text-[#FFB401]">sur Guess </span>
+						<span className="text-[#0B8DFD]">My Draw !</span>
+					</h2>
+					<h2 className="text-[#FE2C65] my-16">Mon compte</h2>
+				</div>
+				<div className="mt-6 mx-auto max-w-xl bg-[#F7F6F6] rounded-3xl p-5 flex justify-between">
+					<div className="flex items-center">
+						<div className="w-40 h-40 overflow-hidden rounded-full">
+							<img
+								src={`${
+									import.meta.env.VITE_BACKEND_URL
+								}/static/${avatarFileName}`}
+								alt="avatar"
+								className="h-full w-full object-cover object-center"
+							/>
+						</div>
+						<div className="text-lg ml-6">
+							<p className="pb-2">
+								Pseudo: <span className="text-gray-500">{user?.pseudo}</span>{" "}
+							</p>
+							<p className="pb-2">
+								Email: <span className="text-gray-500">{user?.email}</span>
+							</p>
+							<p>
+								Mot de passe:{" "}
+								<span className="text-gray-500">************</span>{" "}
+							</p>
+						</div>
+					</div>
+					<div className="self-top text-2xl">
+						<Link to={`/profil/modification/${user?.id_gamer}`}>
+							<button>
+								<FontAwesomeIcon icon={faPen} className="text-[#0B8DFD] me-5" />
+							</button>
+							<button onClick={handleShow}>
+								<FontAwesomeIcon icon={faTrash} className="text-[#FE2C65]" />
+							</button>
+						</Link>
+						{showModal && (
+							<div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+								<div className="bg-white p-8 rounded-lg text-sm">
+									<p>Êtes-vous sûr de vouloir supprimer votre compte ?</p>
+									<div className="mt-10 flex justify-end">
+										<button
+											className="bg-[#FE2C65] text-white px-4 py-2 rounded mr-4 hover:font-bold"
+											onClick={handleDelete}
+										>
+											Supprimer
+										</button>
+										<button
+											className="bg-[#FFB401] text-white px-4 py-2 rounded hover:font-bold"
+											onClick={handleClose}
+										>
+											Annuler
+										</button>
+									</div>
+								</div>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Profil;
