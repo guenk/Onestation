158373f637a54dd1e9581bed8ce9854719274f00
Marion-Chedotal <marion.chedotal@gmail.com>
2024-05-17 11:56:31 +0200

    fix(role): fix delete getIdByLabel

--- backend/src/models/RoleModel.js
+++ backend/src/models/RoleModel.js
@@ -21,6 +21,24 @@ class RoleModel {
 
   /**
    * Récupérer le rôle d'un utilisateur
+   * @param {number} label - le rôle
+   * @returns {Promise<Object>} Une promesse contenant l'id du rôle indiqué.
+   */
+  static async getIdByLabel(label) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT id_role FROM Role WHERE label = ?";
+      db.get(query, [label], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  /**
+   * Récupérer le rôle d'un utilisateur
    * @param {number} idRole - L'identifiant du rôle
    * @returns {Promise<Object>} Une promesse contenant les informations du jeu .
    */
